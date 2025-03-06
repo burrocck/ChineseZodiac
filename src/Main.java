cd0fa03255bd8c6eb4cc7ddb7e342b609f28cb35
Burak <burakkocamaz@hotmail.com>
2025-03-06 22:25:46 +0300

    ChineseZodiac Calculate

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
+        //we get all values from the user
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Doğum yılınızı giriniz: ");
         int birthDay = scanner.nextInt();
-
+        //Chinese calendar written by date of birth
         switch (birthDay % 12) {
             case 0:
                 System.out.println("Maymun");
