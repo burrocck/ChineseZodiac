81be099186391fd5d2eb0be88332a580a26fdf2a
Burak <burakkocamaz@hotmail.com>
2025-03-06 22:22:03 +0300

    ChineseZodiac Calculate

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Doğum yılınızı giriniz: ");
+        int birthDay = scanner.nextInt();
+
+        switch (birthDay % 12) {
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejderya");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+        }
+    }
+}
